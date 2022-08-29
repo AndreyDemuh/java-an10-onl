e1d972d7ad4c625c3e3ac309df3ac1a2ae0ca66b
AndreyDemuh <109171827+AndreyDemuh@users.noreply.github.com>
2022-08-29 15:18:40 +0300

    Delete Worker.java

--- src/Lesson12/HW_12_part_01/Worker.java
+++
@@ -1,9 +0,0 @@
-package Lesson12.HW_12_part_01;
-
-public class Worker extends Employee {
-    public Worker(String name, String surname, String lastname, int age, String specialization,
-                  Report report, int numberCard, String startContract) {
-        super(name, surname, lastname, age, specialization, report, numberCard, startContract);
-    }
-
-}
